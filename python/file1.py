d36e6e5c7d95e483761384e92bf57a6ffee641e3
Muqadas <108210019+21SW066@users.noreply.github.com>
2024-05-26 01:32:40 +0500

    Add files via upload

---
+++ python/file1.py
@@ -0,0 +1,52 @@
+# //Day one
+# // installion of python and editor
+# print("Hello World!")
+# print(5)
+# Day two
+# SHOW 5 PROJECTS BASED ON PYTHON
+# Day three
+# Modules and Pip
+# built-in and external module
+# pip install external-module-name
+# import pandas
+# print("HI")
+# import tensorflow
+# Day four
+# print('Hello World')
+'''
+Day five
+Comments, Escape Sequence ,Characters and print statement
+'''
+# single line comment '#' and for multi line comments '''     '''
+# print('Hi I am good girl \nand this is true')
+# Day Six
+# Variables -->Variables are like container .THey are used store the values in memory.
+# a=1
+# print(a)
+# b='Muqadas'
+# print(type(a))
+# Data Types
+# Day seven
+# Operators--> +, -, *, /, //, %, and 3**2(Exponential)
+# Day eight
+# Calculator in Python
+# c=20
+# d=4
+# print("ADDITION: ",c+d)
+# print("SUBTRACTION: ",c-d)
+# print("MULTIPLICATION: ",c*d)
+# print("DIVISION: ",c/d)
+# print("REMAINDER: ",c%d)
+# print("FLOAT Div: ",c//d)
+# print("EXPONENTION: ",c**d)
+# Day nine
+# Typecasting
+c=input('Enter first number: ')
+d=input('Enter second number: ')
+print("ADDITION: ",int(c)+int(d))
+print("SUBTRACTION: ",int(c)-int(d))
+print("MULTIPLICATION: ",int(c)*int(d))
+print("DIVISION: ",int(c)/int(d))
+print("REMAINDER: ",int(c)%int(d))
+print("FLOAT Div: ",int(c)//int(d))
+print("EXPONENTION: ",int(c)**int(d))
